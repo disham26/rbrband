dde6d5c277c7001db46c8fb8a9802c7afe609019
Prasang Misra <prasang@bonfleet.com>
2018-06-09 17:24:09 +0530

    UIFix

--- public/js/youtube.js
+++ public/js/youtube.js
@@ -9,10 +9,17 @@
                 div = document.createElement("div");
               /*  div.setAttribute("data-id", v[n].dataset.id);
                 div.innerHTML = labnolThumb(v[n].dataset.id);*/
+                  if (v[n].id.startsWith("SoundCloud")){
+                div.setAttribute("data-id", v[n].id);
+                div.innerHTML = labnolThumbSC(v[n].id);
+                div.onclick = labnolIframeSC;
+                v[n].appendChild(div);
+                  }else{
                   div.setAttribute("data-id", v[n].id);
                 div.innerHTML = labnolThumb(v[n].id);
                 div.onclick = labnolIframe;
                 v[n].appendChild(div);
+            }
             }
         });
 
@@ -21,12 +28,25 @@
             play = '<div class="play"></div>';
         return thumb.replace("ID", id) + play;
     }
+    function labnolThumbSC(id) {
+        var thumb = '<img src="https://i1.sndcdn.com/avatars-000324856366-20cmxf-t500x500.jpg">',
+            play = '<div class="playSC"></div>';
+        return thumb + play;
+    }
 
     function labnolIframe() {
         var iframe = document.createElement("iframe");
         var embed = "https://www.youtube.com/embed/ID?autoplay=1";
         iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
         iframe.setAttribute("frameborder", "0");
+        iframe.setAttribute("allowfullscreen", "1");
+        this.parentNode.replaceChild(iframe, this);
+    }
+    function labnolIframeSC() {
+        var iframe = document.createElement("iframe");
+        var embed = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/SCID&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true";
+        iframe.setAttribute("src", embed.replace("SCID", this.dataset.id.split("SoundCloud")[1]));
+        iframe.setAttribute("frameborder", "no");
         iframe.setAttribute("allowfullscreen", "1");
         this.parentNode.replaceChild(iframe, this);
     }
